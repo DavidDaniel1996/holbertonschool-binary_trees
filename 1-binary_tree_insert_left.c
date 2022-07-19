824cca8bf96acfb7fa717b06246109e3c52d705a
Eiden <eidnegabriel07@gmail.com>
2022-07-19 14:04:31 -0400

    make 1 betty compliant

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates a node
+ * binary_tree_insert_left - inserts a node as the left-child of another node
  * @parent: pointer to the parent of the node
  * @value: value to put in new node
  *
@@ -9,34 +9,34 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-  binary_tree_t *temp;
+	binary_tree_t *temp;
 
-  temp = malloc(sizeof(binary_tree_t));
-  if (temp == NULL || parent == NULL || value > parent->n)
-  {
-    return (NULL);
-  }
+	temp = malloc(sizeof(binary_tree_t));
+	if (temp == NULL || parent == NULL || value > parent->n)
+	{
+		return (NULL);
+	}
 
-  temp->n = value;
+	temp->n = value;
 	temp->left = NULL;
 	temp->right = NULL;
 	temp->parent = parent;
 
-  if (parent->left != NULL)
-  {
-    if (temp->n > parent->left->n)
+	if (parent->left != NULL)
+	{
+		if (temp->n < parent->left->n)
 		{
 			return (NULL);
 		}
 
-    temp->left = parent->left;
-    temp->left->parent = temp;
-    parent->left = temp;
-    return (parent->left);
-  }
-  else
-  {
-    parent->left = temp;
-    return (parent->left);
-  }
+		temp->left = parent->left;
+		temp->left->parent = temp;
+		parent->left = temp;
+		return (parent->left);
+	}
+	else
+	{
+		parent->left = temp;
+		return (parent->left);
+	}
 }
