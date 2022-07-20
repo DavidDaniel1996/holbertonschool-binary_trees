93bf56cbcc158c34098095c891bcb64fad6a7e7e
David <4292@holbertonschool.com>
2022-07-20 12:36:18 -0400

    trying to fix output

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,12 +11,20 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *temp;
 
-	temp = malloc(sizeof(binary_tree_t));
-	if (temp == NULL || parent == NULL || value > parent->n)
+	if (parent == NULL || value > parent->n)
 	{
 		return (NULL);
 	}
 
+	if (parent->left != NULL && value < parent->left->n)
+	{
+		return (binary_tree_insert_left(parent->left, value));
+	}
+	temp = malloc(sizeof(binary_tree_t));
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
 	temp->n = value;
 	temp->left = NULL;
 	temp->right = NULL;
