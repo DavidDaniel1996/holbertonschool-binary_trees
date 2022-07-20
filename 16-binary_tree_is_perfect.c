020e00f7e5b894fa134e8b506eadeb6fa8e1d931
David <4292@holbertonschool.com>
2022-07-20 15:16:36 -0400

    possible fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -59,7 +59,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	if ((tree) && (tree->right))
+	if ((tree) && ((tree->right) && (tree->left)))
 	{
 		return (binary_tree_is_perfect(tree->left)
 				&& binary_tree_is_perfect(tree->right));
