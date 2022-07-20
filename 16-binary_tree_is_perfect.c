9eb6283c9f4b3679635ba01787914f358aa17636
Eiden <eidnegabriel07@gmail.com>
2022-07-20 13:39:24 -0400

    task 16 undo last push

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -59,7 +59,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	if (tree)
+	if ((tree) && (tree->right))
 	{
 		return (binary_tree_is_perfect(tree->left)
 				&& binary_tree_is_perfect(tree->right));
