d5e5c500eeda1dbd9303526ff1c2cee42ddc035f
Eiden <eidnegabriel07@gmail.com>
2022-07-21 09:58:51 -0400

    still fixing 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -45,8 +45,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int height_right = binary_tree_height(tree->right);
 	int height_left = binary_tree_height(tree->left);
-	int check_l;
-	int check_r;
 
 	if (tree == NULL)
 	{
@@ -71,7 +69,4 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	}
 		return (1);
-		}
-	}
-	return (0);
 }
